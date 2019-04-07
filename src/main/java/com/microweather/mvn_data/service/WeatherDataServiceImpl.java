c2e71ad3795a2bf49f01aecb0b9dafde08bf6d99
Yan1less <895072956@qq.com>
2019-04-07 11:34:40 +0800

    usr redis cluster

--- src/main/java/com/microweather/mvn_data/service/WeatherDataServiceImpl.java
+++ src/main/java/com/microweather/mvn_data/service/WeatherDataServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisCluster;
 
 import java.io.IOException;
 
@@ -24,9 +25,10 @@ public class WeatherDataServiceImpl implements WeatherDataService {
 	private final static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
 	
 	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
-	
+
+
 	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+	private JedisCluster cluster;
 	
 	@Override
 	public WeatherResponse getDataByCityId(String cityId) {
@@ -45,12 +47,12 @@ public class WeatherDataServiceImpl implements WeatherDataService {
 		String strBody = null;
 		ObjectMapper mapper = new ObjectMapper();
 		WeatherResponse resp = null;
-		ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-		// 先查缓存，缓存有的取缓存中的数据
-		if (stringRedisTemplate.hasKey(key)) {
+
+		if(cluster.exists(key)){
 			logger.info("Redis has data");
-			strBody = ops.get(key);
-		} else {
+			strBody = cluster.get(key);
+		}
+		else {
 			logger.info("Redis don't has data");
 			// 缓存没有，抛出异常
 			throw new RuntimeException("Don't has data!");
